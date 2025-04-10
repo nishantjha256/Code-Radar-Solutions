3a2e6380111b31ffbb3dde4cf49b59738b8e07d6
Nishant Jha <nishant1451.becse24@chitkara.edu.in>
2025-04-10 09:04:24 +0530

    Solution to Input and Print Two Strings

--- Input and Print Two Strings@@6788d5012db25efec946f940/code.c
+++ Input and Print Two Strings@@6788d5012db25efec946f940/code.c
@@ -5,10 +5,13 @@ int main() {
     char str2[100];
 
     printf("Enter two space-separated strings: ");
-    scanf("%s %s",str1, str2);
+    if (scanf("%99s %99s", str1, str2) == 2) {
+        printf("You entered: %s and %s\n", str1, str2);
+    } else {
+        printf("Error: Invalid input. Please enter two space-separated strings.\n");
+        // Optionally, you might want to clear the input buffer here
+        // using a loop with getchar() until a newline is encountered.
+    }
 
-    printf("You entered: %s and %s\n,, str1, str2");
-    
     return 0;
-
 }
